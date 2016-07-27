c4d21af52e06f3d25b42ce5eaf77e7ffcc746d38
Mitesh Shah <mitesh@miteshshah.com>
2016-07-27 16:34:40 +0530

    Added indexing

--- dystic/builder.py
+++ dystic/builder.py
@@ -2,10 +2,12 @@ import main
 import sys
 import os
 import yaml
+import utils
 from marker import Marker
 from templater import Templater
 from configurator import Configurator
-ROOT_DIR = main.config['ROOT_DIR_PATH']
+from indexer import Indexer
+from config import ROOT_DIR_PATH
 
 class Builder:
     """Builds the directory to generate html files."""
@@ -14,12 +16,13 @@ class Builder:
         self.tmplt = Templater()
         self.mrk = Marker()
         self.c = Configurator()
+        self.indx = Indexer()
 
     def build_dir(self, folder):
-        folder_path = os.path.join(ROOT_DIR, folder)
+        folder_path = os.path.join(ROOT_DIR_PATH, folder)
+        print('Building folder: ' + folder_path)
         conf = self.c.get_conf(folder_path)
         default_layout = self.c.get_val(folder_path, 'layout')
-        return
         for root, dirs, files in os.walk(folder_path):
             for f in files:
                 # only supports .md files atm
@@ -32,8 +35,53 @@ class Builder:
                     out_file = os.path.abspath(os.path.join(root, 'index.html'))
                     with open(out_file, 'w') as fp:
                         fp.write(self.tmplt.render(content, layout, metadata))
+                    print('File written: ' + os.path.basename(root))
 
+    def build_index(self, folder):
+        from pprint import pprint
+        folder_path = os.path.join(ROOT_DIR_PATH, folder)
+        nested_dir = self.indx.index_dir(folder_path)
+        # if consequitive directory, file.md does not exists
+        #   list the folder name
+        # else
+        #   list the file title and date
+        default_layout = 'index.html'
+        folder = folder_path.rstrip(os.sep)
+        start = folder.rfind(os.sep) + 1
+        pprint(folder + '' + str(start))
+        for root, dirs, files in os.walk(folder_path):
+            index = {'posts': [], 'collections': []}
+            folders = root[start:].split(os.sep)
+            parent = nested_dir
+            for fold in folders[:-1]:
+                parent = parent.get(fold)
+            parent = parent[folders[-1]]
+            for d in dirs:
+                post = None
+                try:
+                    post = parent[d].get(d+'.md')
+                except KeyError:
+                    pass
+                if post:
+                    # It's a single post
+                    print('Found a post: ' + post['title'])
+                    if post['title'].startswith('"') and post['title'].endswith('"'):
+                        post['title'] = post['title'][1:-1]
+                    post['slug'] = d
+                    index['posts'].append(post)
+                elif not d.startswith('_'):
+                    # It's a collection of posts
+                    print('Found a collection: ' + d)
+                    index['collections'].append({'title': d})
+            if index['posts'] or index['collections']:
+                # print(root, utils.sort_list_dict(index['posts']))
+                out_file = os.path.abspath(os.path.join(root, 'index.html'))
+                with open(out_file, 'w') as fp:
+                    fp.write(self.tmplt.render('', 'index', posts=utils.sort_list_dict(index['posts']), collections=index['collections']))
+                print('File written: ' + os.path.abspath(os.path.basename(root)))
+        return index
 
 if __name__ == '__main__':
-    folder = 'blog/june-15'
+    folder = '.'
     Builder().build_dir(folder)
+    Builder().build_index(folder)
--- dystic/main.py
+++ dystic/main.py
@@ -8,19 +8,22 @@ config = {
     'ROOT_DIR_PATH': os.path.abspath(os.path.dirname(sys.argv[1]))
 }
 
+
 def build(folder):
     builder.Builder().build_dir(folder)
+    builder.Builder().build_index(folder)
+
 
 def cli():
     parser = argparse.ArgumentParser(
-    description='Simple dynamically static blog generator.',
-    formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+        description='Simple dynamically static blog generator.',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('-b', '--build', dest='folder', help="build the specified folder.")
     args = parser.parse_args()
     if args.folder:
-    	build(args.folder)
+        build(args.folder)
     else:
-    	raise SystemExit('Please specify a directory.')
+        raise SystemExit('Please specify a directory.')
 
 def main():
     # with open('_config.yml', 'r') as f:
